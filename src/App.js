485fb2a0e9259b8bed5dfe3af1e1366f30d7d2ea
SeraphShroud <brian.lam@teradata.com>
2020-02-21 15:36:50 -0800

    Initial push with node modules

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Card from './Card'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+
+
+  const [cards, setCards] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("Fish");
+
+  const CARD_QUERY = `https://db.ygoprodeck.com/api/v6/cardinfo.php?race=${query}`;
+  const CARD_SET_QUERY = 'https://db.ygoprodeck.com/api/v6/cardsetsinfo.php?setcode=SDY-046'
+
+  useEffect(() =>{
+    getCards();
+  }, [query]);
+
+  const getCards = async () => {
+    const response = await fetch(CARD_QUERY);
+    const data = await response.json();
+    // console.log(data)
+    setCards(data);
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+    console.log(search)
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("")
+  }
+
+	return (
+		<div className="App">
+			<form onSubmit={getSearch} className="search-form">
+				<input className="search-bar" type="text" value={search} onChange={updateSearch}/>
+        <button classname="search-button" type="submit">
+          Search
+        </button>
+			</form>
+      {cards.map(card => (
+        <Card
+          key={card.name}
+          name={card.name}
+          type={card.race}
+          image={card.card_images[0].image_url_small}
+        />
+      ))};
+		</div>
+	);
+};
+
 
 export default App;

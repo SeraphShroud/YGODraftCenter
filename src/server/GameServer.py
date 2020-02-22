8f0f432596816a754529a423100ae1f3c56b8220
SeraphShroud <brian.lam@teradata.com>
2020-02-21 16:25:03 -0800

    Will test Simple-Game-Server next

---
+++ src/server/GameServer.py
@@ -0,0 +1,120 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Author: Alexandre Mulatinho <alex@mulatinho.net>
+
+import socket
+import select
+import time
+from sys import platform
+
+
+class GameServer(object):
+    socketServer = None
+    user_max = 200
+    connections = {}
+    requests = {}
+    address = {}
+    pollFunc = {}
+    pollEvents = {}
+    pollIn = {}
+    pollOut = {}
+    pollHUP = {}
+
+    def test(self):
+        print("OK")
+
+    def __init__(self):
+        """GameServer is the the TCP/IP server of the pyGameServer"""
+        print("GameServer() init")
+        self.socketServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socketServer.setblocking(0)
+        self.socketServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socketServer.bind(("0.0.0.0", 2020))
+        self.socketServer.listen(self.user_max)
+
+        self.setPollFunction()
+
+    def getSocket(self):
+        return self.socketServer
+
+    def serverLoop(self):
+        try:
+            while True:
+                events = self.pollFunc.poll(1)
+                for fileno, event in events:
+                    print(fileno, event)
+                    if fileno == self.socketServer.fileno():
+                        clientConnection, address = self.socketServer.accept()
+                        clientConnection.setblocking(0)
+
+                        print("connection estabilished from " + str(address))
+
+                        self.pollFunc.register(clientConnection.fileno(), self.pollEvents)
+                        self.connections[clientConnection.fileno()] = clientConnection
+                        self.address[clientConnection.fileno()] = str(address)
+                        self.requests[clientConnection.fileno()] = ''
+
+                    elif event & self.pollIn:
+                        clientData = 'OK'
+                        while True:
+                            print(self.connections[fileno])
+                            try:
+                                clientData = self.connections[fileno].recv(1024)
+                            except socket:
+                                clientData = ''
+
+                            print("len.clientData " + str(len(clientData)))
+                            if len(clientData) > 0:
+                                self.requests[fileno] += clientData
+                            else:
+                                self.connections[fileno].send('"hey man!!"')
+                                self.requests[fileno] = ''
+                                self.pollFunc.modify(fileno, self.pollOut)
+
+                            time.sleep(0.5)
+
+                    elif event & self.pollOut:
+                        self.pollFunc.modify(fileno, self.pollHUP)
+
+                    elif event & self.pollHUP:
+                        print("connection closed from " + str(fileno))
+                        'connection closed from {} at address {}'.format(2, 3)
+                        'connection closed from {} at address {}'.format(str(fileno), self.address[fileno])
+                        self.pollFunc.unregister(fileno)
+                        self.connections[fileno].close()
+                        del self.connections[fileno]
+                        del self.address[fileno]
+
+                print("and the loops goes on, with this connections: " + str(len(self.connections)))
+                time.sleep(0.750)
+        except socket:
+            print(socket)
+        except KeyboardInterrupt:
+            print("Keyboard Interrupt Detected! Shutting down now!")
+        finally:
+            print("Thats it!")
+
+    def setPollFunction(self):
+        """ Set appropriate flags according with OS """
+        if platform == "linux" or platform == "linux2":
+            self.pollFunc = select.epoll()
+            self.pollIn = select.EPOLLIN
+            self.pollOut = select.EPOLLET
+            self.pollHUP = select.EPOLLHUP
+        else:
+            self.pollFunc = select.poll()
+            self.pollIn = select.POLLIN
+            self.pollOut = select.POLLOUT
+            self.pollHUP = select.POLLHUP
+
+        self.pollEvents = self.pollIn | self.pollOut
+        self.pollFunc.register(self.socketServer.fileno(), self.pollEvents)
